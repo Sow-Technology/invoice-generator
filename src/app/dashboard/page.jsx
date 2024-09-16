0b01c61cce1033c23f12f8665ace5a14766f55a0
Vikas Verma <vv4694387@gmail.com>
2024-09-16 23:53:59 +0530

    Filtering records on the basis of store name in dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,18 @@
-import Dashboard from "@/components/pages/Dashboard";
+import DashboardSection from "@/components/pages/Dashboard";
 import React from "react";
 
 const page = () => {
-  return <Dashboard />;
+  return <DashboardSection
+  data={{
+    numberOfOrders: fetchedData.ordersCount,
+    storeSale: fetchedData.totalSales,
+    numberOfCustomers: fetchedData.customersCount,
+    repeatedCustomers: fetchedData.repeatedCustomersCount,
+  }}
+  invoiceData={fetchedInvoiceData}
+  dateRange={dateRange}
+  setDateRange={setDateRange}
+/>;
 };
 
 export default page;
