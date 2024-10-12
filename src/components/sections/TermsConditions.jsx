22c0c37327a2068e8afd9d9979db319961c0b360
Kanish Kumar <kanishkumar11@outlook.in>
2024-10-12 20:28:41 +0530

    minor update fix

--- src/components/sections/TermsConditions.jsx
+++ src/components/sections/TermsConditions.jsx
@@ -9,28 +9,29 @@ export default function TermsConditions() {
       <ol className="list-decimal ml-7">
         <li>
           {" "}
-          Warranty on Frames All frames purchased from Jaanavi Opticals come
-          with a 1-year unconditional warranty. This warranty covers any
-          accidental damage from the customer's end, and we will replace the
+          <b>Warranty on Frames</b> All frames purchased from Jaanavi Opticals
+          come with a 1-year unconditional warranty. This warranty covers any
+          accidental damage from the customer&#39;s end, and we will replace the
           frame free of cost.{" "}
           <li>
-            Warranty on Lenses 1.60 Lenses: Covered under the unconditional
-            warranty. If they break, we will replace them free of cost. Other
-            Lenses: Not covered under the unconditional warranty for breakage.
+            <b>Warranty on Lenses 1.60 Lenses:</b> Covered under the
+            unconditional warranty. If they break, we will replace them free of
+            cost. Other Lenses: Not covered under the unconditional warranty for
+            breakage.
           </li>{" "}
           <li>
-            Warranty on Coatings All lenses have a 1-year warranty on anti-glare
-            and other similar coatings. If any coating peels off within this
-            period, we will replace the lenses free of cost.
+            <b>Warranty on Coatings</b> All lenses have a 1-year warranty on
+            anti-glare and other similar coatings. If any coating peels off
+            within this period, we will replace the lenses free of cost.
           </li>
           <li>
             {" "}
-            Warranty on Sunglasses Polarized Sunglasses: Have a 6-month
-            unconditional warranty. Other Sunglasses: Not covered under any
-            warranty.
+            <b>Warranty on Sunglasses Polarized Sunglasses:</b> Have a 6-month
+            unconditional warranty.
+            <br /> <b>Other Sunglasses:</b> Not covered under any warranty.
           </li>
           <li>
-            Returns and Replacements Please note that we do not offer any
+            <b>Returns and Replacements</b> Please note that we do not offer any
             returns or replacements once the order is confirmed and delivered.{" "}
           </li>
         </li>
